cba6a835597640aa2dc227a86ef6db10d7df75d9
KushnerukAndriy <kushneruk.andriy@gmail.com>
2016-11-16 22:31:48 +0200

    new version whith 2 sensor

--- v1.0/v1.0.ino
+++ v1.0/v1.0.ino
@@ -1,41 +1,68 @@
+#include "DHT.h"
 #include <LiquidCrystal.h>
 #include <LiquidCrystal_I2C.h>
-#include <DHT.h>
-#include <Wire.h> 
-#include <stDHT.h>
 
-DHT sens(DHT21); // Указать датчик DHT11, DHT21 или DHT22. (несколько датчиков вписывать не нужно)
-// Подключать можно только одинаковые датчики, то есть нельзя использовать одновременно DHT11 и DHT22
+LiquidCrystal_I2C lcd(0x27, 20, 4); // Виставляємо адрасе екрану і  тип
 
-void setup() 
-{
-  Serial.begin(57600);
-  pinMode(2, INPUT);
-  digitalWrite(2, HIGH);
-  pinMode(3, INPUT);
-  digitalWrite(3, HIGH);
+DHT dht1(2, DHT22);
+DHT dht2(3, DHT21);
+
+void setup() {
+  lcd.begin(); // вмикаємо екран
+  lcd.backlight(); // вмикаємо підсвітку 
+  lcd.setCursor(1, 0); // загальга назва 
+  lcd.print("Biltong Monitoring"); //  напис;
+  dht1.begin(); 
+  dht2.begin();
 }
 
-void loop() 
-{
-  int t = sens.readTemperature(2); // чтение датчика на пине 2
-  int h = sens.readHumidity(2);    // чтение датчика на пине 2
-  delay(2000);
-  
-  int t2 = sens.readTemperature(3); // чтение датчика на пине 3
-  int h2 = sens.readHumidity(3);    // чтение датчика на пине 3
-  delay(2000);
-  
-  Serial.print("Hum: ");
-  Serial.print(h);
-  Serial.print(" %");
-  Serial.print("Temp: ");
-  Serial.print(t);
-  Serial.println(" C ");
-  Serial.print("Hum2: ");
-  Serial.print(h2);
-  Serial.print(" %");
-  Serial.print("Temp2: ");
-  Serial.print(t2);
-  Serial.println(" C "); 
+void loop() {
+
+  float h1 = dht1.readHumidity();
+  float t1 = dht1.readTemperature();
+  float h2 = dht2.readHumidity();
+  float t2 = dht2.readTemperature();
+
+  if (isnan(t1) || isnan(h1)) {
+    //lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Failed to read from DHT1");
+
+    //    Serial.println("Failed to read from DHT1");
+  }
+  else if (isnan(t2) || isnan(h2)) {
+    //lcd.clear();
+    lcd.setCursor(0, 2);
+    lcd.print("Failed to read from DHT2");
+
+    //    Serial.println("Failed to read from DHT");
+  }
+  else {
+
+    //-----------------dht 1
+    lcd.setCursor(0, 1);
+    lcd.print("h1=");
+    lcd.print(h1);
+    lcd.print("%");
+    lcd.print(" t1=");
+    lcd.print(t1);
+    lcd.print("C");
+    //-----------------dht 2
+    lcd.setCursor(0, 2);
+    lcd.print("h2=");
+    lcd.print(h2);
+    lcd.print("%");
+    lcd.print(" t2=");
+    lcd.print(t2);
+    lcd.print("C");
+    // serredne
+    lcd.setCursor(1, 3);
+    lcd.print("h=");
+    lcd.print((h1+h2)/2);
+    lcd.print("%");
+    lcd.setCursor(10, 3);
+    lcd.print(" t=");
+    lcd.print((t1+t2)/2);
+    lcd.print("C");
+  }
 }
