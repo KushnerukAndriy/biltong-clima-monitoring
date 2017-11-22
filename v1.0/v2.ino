24e8c52e90a7c069dc4ec68e2f911a22f1d237ef
Andriy Kushneruk <kushneruk.andriy@gmail.com>
2017-11-23 01:22:46 +0200

    Update v2.ino

--- v1.0/v2.ino
+++ v1.0/v2.ino
@@ -1,20 +1,20 @@
-/*
-You will need this 
-https://github.com/bborncr/nextion
-and this
-http://openhardware.gridshield.net/home/nextion-lcd-getting-started-for-arduino
-*/
-
 #include <SoftwareSerial.h>
-#include <Nextion.h>
+#include <Nextion.h>  // https://github.com/bborncr/nextion
+#include <iarduino_RTC.h> //http://iarduino.ru/file/235.html 0x68 http://wiki.iarduino.ru/page/chasy-realnogo-vremeni-rtc-trema-modul/
 
 SoftwareSerial nextion(2, 3);
+iarduino_RTC time(RTC_DS1307);
 
 Nextion myNextion(nextion, 9600);
 
 void setup() {
+  delay(100);
   Serial.begin(9600);
   myNextion.init();
+  time.begin();
+  pinMode(LED_BUILTIN, OUTPUT);
+  //time.settime(0,37,00,23,11,17,4);  // 0s,37min,00h, 23/11/17, thu  --- uncomennt only when set time!!
+  
 }
 
 void loop() {
@@ -23,3 +23,29 @@ void loop() {
     Serial.println(message);
   }  
 }
+
+//    if(millis()%1000==0){ // если прошла 1 секунда
+//    Serial.println(time.gettime("d-m-Y, H:i:s, D")); // выводим время
+//    delay(1); // приостанавливаем на 1 мс, чтоб не выводить время несколько раз за 1мс
+//  }
+
+
+//ventilation
+//vt0 --- 10/40
+//vt1 --- 10/20
+//vt2 --- on
+//vt4 --- off
+//humidity
+//68 48,0,0 --- middle
+//68 49,0,0 --- hight
+//68 50,0,0 --- off
+//lamp
+//lt0 --- auto
+//lt1 --- on
+//lt2 --- off
+//
+//    if (message == "lt1"){
+//        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//        delay(5000);                       // wait for a second
+//        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//    }
