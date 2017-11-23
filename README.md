9a8e6ffc0fabafcbc30d9a7641245f84b4ef0231
Andriy Kushneruk <akushneruk@akushneruk.local>
2017-11-24 00:27:38 +0200

    clear code

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # biltong-clima-monitoring
+
+
+You need:
+- <Nextion.h> https://github.com/bborncr/nextion
+- <iarduino_RTC.h> //http://iarduino.ru/file/235.html --- http://wiki.iarduino.ru/page/chasy-realnogo-vremeni-rtc-trema-modul/
+
+
+Commands frome display:
+- ventilation
+    vt0 --- 10/40
+    vt1 --- 10/20
+    vt2 --- on
+    vt4 --- off
+- humidity
+    68 48,0,0 --- middle
+    68 49,0,0 --- hight
+    68 50,0,0 --- off
+- lamp
+    lt0 --- auto
+    lt1 --- on
+    lt2 --- off
--- v001.ino
+++ v001.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
-#include <Nextion.h>  // https://github.com/bborncr/nextion
-#include <iarduino_RTC.h> //http://iarduino.ru/file/235.html 0x68 http://wiki.iarduino.ru/page/chasy-realnogo-vremeni-rtc-trema-modul/
+#include <Nextion.h>
+#include <iarduino_RTC.h>
 
 SoftwareSerial nextion(2, 3);
 iarduino_RTC time(RTC_DS1307);
@@ -23,29 +23,3 @@ void loop() {
     Serial.println(message);
   }  
 }
-
-//    if(millis()%1000==0){ // если прошла 1 секунда
-//    Serial.println(time.gettime("d-m-Y, H:i:s, D")); // выводим время
-//    delay(1); // приостанавливаем на 1 мс, чтоб не выводить время несколько раз за 1мс
-//  }
-
-
-//ventilation
-//vt0 --- 10/40
-//vt1 --- 10/20
-//vt2 --- on
-//vt4 --- off
-//humidity
-//68 48,0,0 --- middle
-//68 49,0,0 --- hight
-//68 50,0,0 --- off
-//lamp
-//lt0 --- auto
-//lt1 --- on
-//lt2 --- off
-//
-//    if (message == "lt1"){
-//        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-//        delay(5000);                       // wait for a second
-//        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-//    }
