2d6fbf7513dd215c606a23183365b6e82e7a7e5f
Andriy Kushneruk <kushneruk.andriy@gmail.com>
2017-11-30 13:36:22 +0200

    last

--- v001/v001.ino
+++ v001/v001.ino
@@ -9,28 +9,28 @@ Nextion myNextion(nextion, 9600);
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 RTC_DS1307 rtc;
 int* configs[4];
-
 uint32_t refTime;
 uint32_t refIndex = 0;
 int* currentConfig;
+int currentConfigIndex = 0;
 
 void setup() {
   delay(1000);
   Serial.begin(9600);
   rtc.begin();
   myNextion.init();
-
+  refTime = rtc.now().unixtime();
+  
   if ( (! sht31.begin(0x44)) or (! rtc.isrunning()) ){
     Serial.println("Somethinks wrong!!");
     while (1) delay(1);
   }
+  
+  configs[0] = new int[3] {1, 0, 0};
+  configs[1] = new int[5] {2, 60, 95, 240, 0}; 
+  configs[2] = new int[5] {2, 60, 95, 120, 0}; 
+  configs[3] = new int[3] {1, 0, 95};
 
-  configs[0] = new int[3] {1, 0, 0};             // перший параметр - це кількість значень, далі значення ідуть парами - 1 число кількість часу яке має пройти до переключення режиму, 2 число значення режиму
-  configs[1] = new int[5] {2, 600, 95, 2400, 255}; // 45 120 це ті значення які попадуть у змінну вольтаж currentVoltage, 
-  configs[2] = new int[5] {2, 600, 95, 1200, 255}; 
-  configs[3] = new int[3] {1, 0, 255};
-
-  refTime = rtc.now().unixtime();
 }
 
 void setupFan(int* currentConfig) {
@@ -42,26 +42,29 @@ void setupFan(int* currentConfig) {
   
   uint32_t refDiff = currentTime - refTime;
   if(refDiff >= currentDelay) {
-      refIndex++;
-      if(refIndex >= arrSize) refIndex = 0;
-      refTime = currentTime;
+    refIndex++;
+    if(refIndex >= arrSize) refIndex = 0;
+    refTime = currentTime;
   }
- 
+  
   analogWrite(5, currentVoltage);
+  analogWrite(6, currentVoltage);
 }
 
-int currentConfigIndex = 0;
-
 void loop() {
-   String message = myNextion.listen();
-   if(message != ""){
-     Serial.println(message);
-   }
-
-  setupFan(configs[currentConfigIndex]);
-
+  String message = myNextion.listen();
   int t = sht31.readTemperature(); // float for real
   int h = sht31.readHumidity();
+  if(message != ""){
+    Serial.println(message);
+  }
+  
+  if ( message == "vt0" ) currentConfigIndex = 1;
+  if ( message == "vt1" ) currentConfigIndex = 2;
+  if ( message == "vt2" ) currentConfigIndex = 3;
+  if ( message == "vt4" ) currentConfigIndex = 0;
+
+  setupFan(configs[currentConfigIndex]);
 
   if ( (! isnan(t)) and (! isnan(h)) ) {
    delay(200);
