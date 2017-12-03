f7cbfb42c39f6a789e5a6aa30aa3b221b69ccba6
Andriy Kushneruk <kushneruk.andriy@gmail.com>
2017-12-03 23:53:17 +0200

    time problem

--- v001/v001.ino
+++ v001/v001.ino
@@ -1,36 +1,56 @@
 #include <SoftwareSerial.h>
 #include <Nextion.h>
-#include "RTClib.h"
 #include <Wire.h>
+#include "RTClib.h"
 #include "Adafruit_SHT31.h"
+//#include <Arduino.h>
 
-SoftwareSerial nextion(2, 3);
+SoftwareSerial nextion(3, 2);
 Nextion myNextion(nextion, 9600);
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
 RTC_DS1307 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 int* configs[4];
 uint32_t refTime;
 uint32_t refIndex = 0;
 int* currentConfig;
 int currentConfigIndex = 0;
 
+int CH1 = 12;
+int CH2 = 11;
+int CH3 = 10;
+int CH4 = 9;
+
 void setup() {
+  while (!Serial);
   delay(1000);
   Serial.begin(9600);
-  rtc.begin();
-  myNextion.init();
-  refTime = rtc.now().unixtime();
   
-  if ( (! sht31.begin(0x44)) or (! rtc.isrunning()) ){
-    Serial.println("Somethinks wrong!!");
+  if ( (! sht31.begin(0x44)) and (! rtc.begin()) ) {
+    Serial.println("Couldn't find SHT31 or couldn't find RTC");
     while (1) delay(1);
   }
-  
-  configs[0] = new int[3] {1, 0, 0};
-  configs[1] = new int[5] {2, 60, 95, 240, 0}; 
-  configs[2] = new int[5] {2, 60, 95, 120, 0}; 
-  configs[3] = new int[3] {1, 0, 95};
+  else {
+    Serial.println("RTC SHT31 --- OK");
+  }
 
+  myNextion.init();
+  refTime = rtc.now().unixtime();
+  configs[0] = new int[3] {1, 0, 0};
+  configs[1] = new int[5] {2, 6, 1, 24, 0}; // mode number, time1, value1, time2, value2
+  configs[2] = new int[5] {2, 6, 1, 12, 0}; 
+  configs[3] = new int[3] {1, 0, 95};
+  
+  pinMode(CH1, OUTPUT);
+  pinMode(CH2, OUTPUT);
+  pinMode(CH3, OUTPUT);
+  pinMode(CH4, OUTPUT);
+  digitalWrite(CH1, 1);
+  digitalWrite(CH2, 1);
+  digitalWrite(CH3, 1);
+  digitalWrite(CH4, 1);
 }
 
 void setupFan(int* currentConfig) {
@@ -46,32 +66,37 @@ void setupFan(int* currentConfig) {
     if(refIndex >= arrSize) refIndex = 0;
     refTime = currentTime;
   }
-  
-  analogWrite(5, currentVoltage);
-  analogWrite(6, currentVoltage);
+  digitalWrite(CH1, 1);
 }
 
-void loop() {
-  String message = myNextion.listen();
-  int t = sht31.readTemperature(); // float for real
+void loop () {
+  delay(1000);
+  int t = sht31.readTemperature();
   int h = sht31.readHumidity();
+  String message = myNextion.listen();
   if(message != ""){
     Serial.println(message);
   }
-  
-  if ( message == "vt0" ) currentConfigIndex = 1;
-  if ( message == "vt1" ) currentConfigIndex = 2;
-  if ( message == "vt2" ) currentConfigIndex = 3;
-  if ( message == "vt4" ) currentConfigIndex = 0;
-
-  setupFan(configs[currentConfigIndex]);
 
   if ( (! isnan(t)) and (! isnan(h)) ) {
-   delay(200);
    myNextion.setComponentText("t0", String(t));
    myNextion.setComponentText("t1", String(h));
+   Serial.println( t);
+   Serial.println( h);
   } 
   else { 
    Serial.println("Failed to read temperature");
   }
+  setupFan(configs[1]);
 }
+
+
+//  
+//  if ( message == "vt0" ) currentConfigIndex = 1;
+//  if ( message == "vt1" ) currentConfigIndex = 2;
+//  if ( message == "vt2" ) currentConfigIndex = 3;
+//  if ( message == "vt4" ) currentConfigIndex = 0;
+//
+//  setupFan(configs[currentConfigIndex]);
+//
+
