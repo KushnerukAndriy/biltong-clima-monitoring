c81f4acf6c69675349d23ca22836d2609f772b9c
Andriy Kushneruk <kushneruk.andriy@gmail.com>
2017-12-05 14:41:30 +0200

    fix some changes

--- v001/v001.ino
+++ v001/v001.ino
@@ -3,20 +3,12 @@
 #include <Wire.h>
 #include "RTClib.h"
 #include "Adafruit_SHT31.h"
-//#include <Arduino.h>
 
 SoftwareSerial nextion(3, 2);
 Nextion myNextion(nextion, 9600);
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 RTC_DS1307 rtc;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-int* configs[4];
-uint32_t refTime;
-uint32_t refIndex = 0;
-int* currentConfig;
-int currentConfigIndex = 0;
 
 int CH1 = 12;
 int CH2 = 11;
@@ -27,6 +19,7 @@ void setup() {
   while (!Serial);
   delay(1000);
   Serial.begin(9600);
+  myNextion.init();
   
   if ( (! sht31.begin(0x44)) and (! rtc.begin()) ) {
     Serial.println("Couldn't find SHT31 or couldn't find RTC");
@@ -35,13 +28,6 @@ void setup() {
   else {
     Serial.println("RTC SHT31 --- OK");
   }
-
-  myNextion.init();
-  refTime = rtc.now().unixtime();
-  configs[0] = new int[3] {1, 0, 0};
-  configs[1] = new int[5] {2, 6, 1, 24, 0}; // mode number, time1, value1, time2, value2
-  configs[2] = new int[5] {2, 6, 1, 12, 0}; 
-  configs[3] = new int[3] {1, 0, 95};
   
   pinMode(CH1, OUTPUT);
   pinMode(CH2, OUTPUT);
@@ -51,22 +37,6 @@ void setup() {
   digitalWrite(CH2, 1);
   digitalWrite(CH3, 1);
   digitalWrite(CH4, 1);
-}
-
-void setupFan(int* currentConfig) {
-  uint32_t currentTime = rtc.now().unixtime();
-  
-  int arrSize = currentConfig[0];
-  uint32_t currentDelay = currentConfig[1 + refIndex * 2 ];
-  uint32_t currentVoltage = currentConfig[1 + refIndex * 2 + 1];
-  
-  uint32_t refDiff = currentTime - refTime;
-  if(refDiff >= currentDelay) {
-    refIndex++;
-    if(refIndex >= arrSize) refIndex = 0;
-    refTime = currentTime;
-  }
-  digitalWrite(CH1, 1);
 }
 
 void loop () {
@@ -87,7 +57,6 @@ void loop () {
   else { 
    Serial.println("Failed to read temperature");
   }
-  setupFan(configs[1]);
 }
 
 
